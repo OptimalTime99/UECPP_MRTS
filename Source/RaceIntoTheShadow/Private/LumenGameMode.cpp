33d010f294d66591ae43384cee3152e25fac5813
optimal <optimal9744@gmail.com>
2026-01-19 21:03:08 +0900

    [➕ add] PlayerController 클래스 생성 및 GameMode에 지정

--- Source/RaceIntoTheShadow/Private/LumenGameMode.cpp
+++ Source/RaceIntoTheShadow/Private/LumenGameMode.cpp
@@ -1,7 +1,10 @@
 ﻿#include "LumenGameMode.h"
 #include "RITSCharacter.h"
+#include "RITSPlayerController.h"
 
 ALumenGameMode::ALumenGameMode()
 {
     DefaultPawnClass = ARITSCharacter::StaticClass();
+
+    PlayerControllerClass = ARITSPlayerController::StaticClass();
 }
---
+++ Source/RaceIntoTheShadow/Public/RITSPlayerController.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RITSPlayerController.generated.h"
+
+UCLASS()
+class RACEINTOTHESHADOW_API ARITSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
