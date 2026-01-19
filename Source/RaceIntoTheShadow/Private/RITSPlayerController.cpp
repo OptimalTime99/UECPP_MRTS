126923dde7ad0a5c8566e97a7b883292e4e46c2c
optimal <optimal9744@gmail.com>
2026-01-19 21:17:54 +0900

    [ 🌟 Update] C++ PlayerController 클래스에서 IA, IMC를 리플랙션 시스템에 등록 및 nullptr로 초기화
    - C++ PlayerController 클래스에서 IMC를 가장 높은 우선순위로 로컬 플레이어한테 활성화
    - BP_PlayerController 클래스에서 IMC, IA 연결

--- Source/RaceIntoTheShadow/Private/RITSPlayerController.cpp
+++ Source/RaceIntoTheShadow/Private/RITSPlayerController.cpp
@@ -1,1 +1,28 @@
 ﻿#include "RITSPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+ARITSPlayerController::ARITSPlayerController() : 
+    InputMappingContext(nullptr),
+    MoveAction(nullptr),
+    JumpAction(nullptr),
+    SprintAction(nullptr),
+    LookAction(nullptr) {}
+
+
+void ARITSPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+            LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+        {
+            if (InputMappingContext)
+            {
+                Subsystem->AddMappingContext(InputMappingContext, 0);
+
+            }
+        }
+    }
+}
--- Source/RaceIntoTheShadow/Public/RITSPlayerController.h
+++ Source/RaceIntoTheShadow/Public/RITSPlayerController.h
@@ -4,9 +4,32 @@
 #include "GameFramework/PlayerController.h"
 #include "RITSPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class RACEINTOTHESHADOW_API ARITSPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+    ARITSPlayerController();
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputMappingContext* InputMappingContext;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputAction* MoveAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputAction* JumpAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputAction* SprintAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputAction* LookAction;
+
+    virtual void BeginPlay() override;
 };
