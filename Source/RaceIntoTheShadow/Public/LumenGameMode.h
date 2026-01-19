31ad7b0a22084157347cb8ff18c57593141853cc
optimal <optimal9744@gmail.com>
2026-01-19 20:29:13 +0900

    [➕ Add] GameMode 파일 생성

---
+++ Source/RaceIntoTheShadow/Public/LumenGameMode.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LumenGameMode.generated.h"
+
+UCLASS()
+class RACEINTOTHESHADOW_API ALumenGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
