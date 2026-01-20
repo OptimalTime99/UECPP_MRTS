207ea58cbf519d66973e59a1e089fab00575d7fa
optimal <optimal9744@gmail.com>
2026-01-20 12:37:49 +0900

    [➕ Add] 이동 액터 생성 및 구현

---
+++ Source/RaceIntoTheShadow/Private/RITS_Move_Actor.cpp
@@ -0,0 +1,43 @@
+﻿#include "RITS_Move_Actor.h"
+
+ARITS_Move_Actor::ARITS_Move_Actor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
+
+	MoveSpeed = 400.0f;
+	MaxRange = 500.0f;
+}
+
+void ARITS_Move_Actor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();
+	MoveDirection = GetActorForwardVector();
+}
+
+void ARITS_Move_Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector CurrentLocation = GetActorLocation();
+	FVector NewLocation = CurrentLocation + (MoveDirection * MoveSpeed * DeltaTime);
+
+	SetActorLocation(NewLocation);
+
+	Dist = FVector::Dist(StartLocation, NewLocation);
+
+	if (Dist >= MaxRange)
+	{
+		MoveDirection = -MoveDirection;
+	}
+	
+
+}
+
---
+++ Source/RaceIntoTheShadow/Public/RITS_Move_Actor.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RITS_Move_Actor.generated.h"
+
+class USceneComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class RACEINTOTHESHADOW_API ARITS_Move_Actor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ARITS_Move_Actor();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	USceneComponent* SceneRoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move")
+	float MoveSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move")
+	float MaxRange;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	float Dist;
+	FVector StartLocation;
+	FVector MoveDirection;
+
+};
