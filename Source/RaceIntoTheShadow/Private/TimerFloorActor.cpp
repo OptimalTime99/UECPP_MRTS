c201be87d9f79ae368b418f69a2005c1e027ebfb
optimal <optimal9744@gmail.com>
2026-01-20 14:25:59 +0900

    [➕ Add] 타이머 발판 액터 생성 및 구현

---
+++ Source/RaceIntoTheShadow/Private/TimerFloorActor.cpp
@@ -0,0 +1,46 @@
+﻿#include "TimerFloorActor.h"
+
+ATimerFloorActor::ATimerFloorActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
+}
+
+void ATimerFloorActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ScheduleStateChange(false);
+}
+
+void ATimerFloorActor::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	GetWorld()->GetTimerManager().ClearTimer(BlinkTimerHandle);
+}
+
+void ATimerFloorActor::ScheduleStateChange(bool bShow)
+{
+	float WaitTime = bShow ? HiddenDuration : VisibleDuration;
+
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	TWeakObjectPtr<ATimerFloorActor> WeakThis(this);
+
+	TimerManager.SetTimer(BlinkTimerHandle, [WeakThis, bShow]()
+		{
+			if (ATimerFloorActor* StrongThis = WeakThis.Get())
+			{
+				StrongThis->SetActorHiddenInGame(!bShow);
+				StrongThis->SetActorEnableCollision(bShow);
+				StrongThis->ScheduleStateChange(!bShow);
+			}
+		}, WaitTime, false);
+}
+
---
+++ Source/RaceIntoTheShadow/Public/TimerFloorActor.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TimerFloorActor.generated.h"
+
+class USceneComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class RACEINTOTHESHADOW_API ATimerFloorActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ATimerFloorActor();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	USceneComponent* SceneRoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	UStaticMeshComponent* StaticMeshComp;
+
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Blinking")
+	float VisibleDuration = 3.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Blinking")
+	float HiddenDuration = 2.0f;
+
+	FTimerHandle BlinkTimerHandle;
+
+	void ScheduleStateChange(bool bShow);
+
+};
