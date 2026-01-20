90ad49d95bea32f67eabb51dd67d5dda3ee8008b
optimal <optimal9744@gmail.com>
2026-01-20 12:53:55 +0900

    [➕ Add] 회전 액터 생성 및 구현

---
+++ Source/RaceIntoTheShadow/Private/RITS_Rotate_Actor.cpp
@@ -0,0 +1,35 @@
+﻿#include "RITS_Rotate_Actor.h"
+
+ARITS_Rotate_Actor::ARITS_Rotate_Actor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
+}
+
+void ARITS_Rotate_Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ARITS_Rotate_Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float RotateValue = RotationSpeed * DeltaTime;
+
+	if (bReverseRotation)
+	{
+		RotateValue *= -1.0f;
+	}
+
+	FRotator NewRotation = FRotator(0.0f, RotateValue, 0.0f);
+
+	AddActorLocalRotation(NewRotation);
+}
+
---
+++ Source/RaceIntoTheShadow/Public/RITS_Rotate_Actor.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RITS_Rotate_Actor.generated.h"
+
+class USceneComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class RACEINTOTHESHADOW_API ARITS_Rotate_Actor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ARITS_Rotate_Actor();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	USceneComponent* SceneRoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	float RotationSpeed = 60.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	bool bReverseRotation = false;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+
+public:	
+	
+
+};
